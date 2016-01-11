6cb6b914fe3af5592691dae75ff6d7b11602bd96
SNYdev <yanis.sahnoune@supinternet.fr>
2016-01-11 21:18:18 +0100

    add searchDocument

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,26 +9,31 @@ class Main{
 	static boolean finished = false;
 	
 	public static void main(String args[]){
-		
-		Scanner sc =  new Scanner(System.in);
-		System.out.println("1 = Inscription, 2 = Louer un livre, 3 = rendre un livre, 4 = afficher les livres utilisateurs, 5 = rechercher un document");
-		int resp = sc.nextInt();
-		while(Main.finished == false){	
+		while(Main.finished == false){
+			Scanner sc =  new Scanner(System.in);
+			System.out.println("1 = Inscription, 2 = Louer un livre, 3 = rendre un livre, 4 = afficher les livres utilisateurs, 5 = rechercher un document 6 = eteindre");
+			int resp = sc.nextInt();
+			Library myLibrary = new Library();
 			if(resp == 1){
-				System.out.println("Nom du membre");
 				String se = sc.nextLine();
+					myLibrary.addMember(se);
 				} else if(resp == 2) {
-					
-					
+					System.out.println("Nom du livre");
 				} else if(resp == 3){
-					
+					System.out.println("Nom du livre à rendre");
+	
 				} else if(resp == 4){
-				
+					System.out.println("Nom du membre");
+	
 				} else if(resp == 5){
-					
-			} else {
-				System.out.println("La fonction n'existe pas");
+					myLibrary.searchDocument();
+				} else if(resp == 6){
+					System.out.println("Bye");
+					Main.finished = true;
+				} else {
+					System.out.println("La fonction n'existe pas");
 			}
 		}
+			
 	}
 }
